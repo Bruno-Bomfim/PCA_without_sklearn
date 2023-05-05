ca768cff6fdbca4c51fab82562e7a9cc004e4f2e
Bruno Cavalcante Bomfim <bruno.bomfim@poli.ufrj.br>
2023-05-05 14:13:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 def main():
 #importa o arquivo do excel
-    arquivo = str(input('Digite o local do arquivo: '))
+    arquivo = str(input('Digite o local do arquivo(arquivo excel): '))
     dados = create.PCA(pd.read_excel(arquivo))
 #aplica pca na base de dados
     dados.pca(3)
